4d6b8977692193dc826db8a79bba2a4feef5722e
Simon Löschke <simon-loeschke@posteo.de>
2023-08-06 11:11:57 +0200

    detailansicht um beschreibung ergänzt

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include 
 from django.conf import  settings
 from django.conf.urls.static import static
 
@@ -24,6 +24,7 @@ from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('item/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
