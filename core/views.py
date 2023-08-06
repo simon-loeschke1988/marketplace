98b939fe7390c1a2d005e71a6ca98490419f8f68
Simon Löschke <simon-loeschke@posteo.de>
2023-08-06 19:05:47 +0200

    backend registrierung

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from item.models import Item, Category
 from .forms import SignupForm
 # Create your views here.
@@ -15,5 +15,13 @@ def contact(request):
 # User registrieren
 
 def signup(request):
-    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            # User erstellen
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    
     return render(request, 'core/signup.html', {'form':form})
