fac10c7c83db751d6d70ec63af44dee52ce61da9
Simon Löschke <simon-loeschke@posteo.de>
2023-08-06 23:47:02 +0200

    Artikel bearbeiten funktion

--- item/forms.py
+++ item/forms.py
@@ -21,3 +21,19 @@ class NewItemForm(forms.ModelForm):
             'image': forms.FileInput(attrs={
                 'class': INPUT_CLASSES}),
         }
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['name','description','price','image','is_sold']
+        
+        # Hier eine andere Art, Formulare zu erstellen
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES}),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES}),
+            'price': forms.NumberInput(attrs={
+                'class': INPUT_CLASSES}),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES}),
+        }
