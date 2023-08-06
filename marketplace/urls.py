c0ad2a7ff63555e35a7407fcccfa4a0d1d4b4830
Simon Löschke <simon-loeschke@posteo.de>
2023-08-06 10:16:56 +0200

    static files for development

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import  settings
+from django.conf.urls.static import static
 
 # die views einbinden
 from core.views import index, contact
@@ -24,4 +26,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
