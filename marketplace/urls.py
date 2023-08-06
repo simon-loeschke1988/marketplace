1061ce01409937605a1e633ce6c4a2aa3f4530c1
Simon Löschke <simon-loeschke@posteo.de>
2023-08-06 23:07:31 +0200

    dashboard programmiert

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('', include('core.urls')),
     #die item-urls einbinden
     path('item/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
